190ec57e70e53a7d763c95bcc72cbc94b5b077a4
fahrizalrakhagunawan <agoentea30@gmail.com>
2024-11-13 13:35:04 +0700

    Menambahkan validasi untuk input panjang dan lebar

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 # main.py
 
-# meminta input panjang dan lebar dari pengguna
-panjang = float(input("masukkan panjang persegi panjang"))
-lebar = float(input("masukkan lebar persegi panjang"))
+# fungsi untuk memeriksa apakah input adalah bilangan positif
+def input_positif(prompt):
+    while true:
+        try:
+            nilai - float(input(prompt))
+            if nilai > 0:
+                return nilai
+            else:
+                print("nilai harus lebih dari 0. coba lagi.")
+     except valueerror:
+        print("input tidak valid. masukkan angka.")
 
-# menghitung luas
+# meminta input panjang dan lebar dari pengguna dengan validasi
+panjang = input_positif("masukkan panjang persegi panjang")
+lebar = input_positif("masukkan lebar persegi panjang:")
+
+# menghitung luas 
 luas = panjang * lebar
-
-# menghitung keliling
-keliling = 2 * (panjang + lebar)
-
+# menghitung keliling 
+keliling = 2 *(panjang +lebar)
 
 # menampilkan hasil luas dan keliling dengan format yang lebih jelas
 print(f"luas persegi panjang adalah {luas} satuan persegi")
 print(f"keliling persegi panjang adalah {keliling} satuan")
+
 
